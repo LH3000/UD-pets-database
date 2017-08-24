213d4d75bbe143d2fde29392d41cc7cf69dd7df6
Lewei.H <hexxx373@umn.edu>
2017-08-24 13:11:57 -0500

    Implement ContentProvider Query() method

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,8 +1,11 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 /**
@@ -15,6 +18,34 @@ public class PetProvider extends ContentProvider {
      * Tag for the log messages
      */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    /**
+     * URI matcher code for te content URI for the pets table
+     */
+    public static final int PETS = 100;
+    /**
+     * URI matcher code for te content URI for a single pet the pets table
+     */
+    public static final int PET_ID = 101;
+    /**
+     * URI matcher object to match a context URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI
+     * common to use NO_MATCH here
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // static initializer. This is run the first time anything is called from this class
+    static {
+        // the content URI of the form "content://com.example.android.pets/pets" will map to the
+        // integer code {@link #PETS}. This URI is used to provide access to multiple rows of the
+        // pets table
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+
+        // the content URI of the form "content://com.example.android.pets/pets/#" will map to the
+        // integer code {@link #PETS_ID}. This URI is used to provide access to ONE single row
+        // of the pets table.
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
+    }
 
     private PetDBHelper mDbHelper;
 
@@ -33,7 +64,42 @@ public class PetProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+
+        // get the db object
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // build cursor to hold the result of the query
+        Cursor cursor;
+
+        // figure out if the uri matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // query the db directly with the URI
+                cursor = database.query(PetContract.PetEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            case PET_ID:
+                // For this case, extract out the ID from the URI.
+                // for an example URI such as "content://com.example.android.pets/pets/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = PetContract.PetEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                // this will perform a query on the pets table where _id = 3 to return a cursor row
+                // that contains that row
+                cursor = database.query(PetContract.PetEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     /**
@@ -45,7 +111,7 @@ public class PetProvider extends ContentProvider {
     }
 
     /**
-     * Updates the data at the given selection and selection arguments, with the new ContentValues.
+     * Updates the data at the x  selection and selection arguments, with the new ContentValues.
      */
     @Override
     public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
