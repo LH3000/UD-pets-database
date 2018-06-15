737e71012623055e6010b50e94373cbfb655fafa
Lewei.H <hexxx373@umn.edu>
2018-06-15 14:18:30 -0500

    Prevent crash by empty inputs & weight

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -147,11 +147,24 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         String nameString = mNameEditText.getText().toString().trim();
         String breedString = mBreedEditText.getText().toString().trim();
         String weightString = mWeightEditText.getText().toString().trim();
-        int weight = Integer.parseInt(weightString);
+
+        // Check if the input fields are empty, if yes then exit activity (cancel action)
+        if (mCurrentPetUri == null && TextUtils.isEmpty(nameString)
+                && TextUtils.isEmpty(breedString) &&
+                TextUtils.isEmpty(weightString) && mGender == PetEntry.GENDER_UNKNOWN) {
+            return;
+        }
 
         // create a ContentValue object where column names are the keys,
         // and pet attributes from the editor are the values
         ContentValues values = new ContentValues();
+
+        // if the weight is not provided by the user, don't try to parse the string
+        // but to use 0 by default
+        int weight = 0;
+        if (!TextUtils.isEmpty(weightString)) {
+            weight = Integer.parseInt(weightString);
+        }
         values.put(PetEntry.COLUMN_PET_NAME, nameString);
         values.put(PetEntry.COLUMN_PET_BREED, breedString);
         values.put(PetEntry.COLUMN_PET_GENDER, mGender);
